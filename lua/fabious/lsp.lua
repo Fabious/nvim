303ba55094896090eb42495affefa545f71229eb
fabious <fabious@free.fr>
2022-03-23 21:20:05 +0100

    add mapping for formatting

--- lua/fabious/lsp.lua
+++ lua/fabious/lsp.lua
@@ -17,6 +17,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', [[<cmd>lua vim.lsp.buf.formatting()<CR>]], opts)
 end
 
 -- nvim-cmp supports additional completion capabilities
